03d205f0637f606d79e58a490946f0871bf46376
Leon <cy.tcl@qq.com>
2023-04-28 15:25:38 +0800

    Initial commit

---
+++ app/src/main/java/com/sunnyweather/android/logic/Repository.kt
@@ -0,0 +1,28 @@
+package com.sunnyweather.android.logic
+
+import androidx.lifecycle.liveData
+import com.sunnyweather.android.logic.model.Place
+import com.sunnyweather.android.logic.network.SunnyWeatherNetwork
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+import kotlin.coroutines.CoroutineContext
+
+object Repository {
+
+    fun searchPlaces(query: String) = liveData(Dispatchers.IO) {
+        var result = try {
+            val placeResponse = SunnyWeatherNetwork.searchPlaces(query)
+            if (placeResponse.status == "ok") {
+                val places = placeResponse.places
+                Result.success(places)
+            } else {
+                Result.failure(RuntimeException("response status is ${placeResponse.status}"))
+            }
+        }catch (e: Exception){
+            Result.failure<List<Place>>(e)
+        }
+        emit(result)
+    }
+
+}
---
+++ app/src/main/java/com/sunnyweather/android/ui/place/PlaceViewModel.kt
@@ -0,0 +1,21 @@
+package com.sunnyweather.android.ui.place
+
+import androidx.lifecycle.*
+import androidx.lifecycle.Transformations.*
+import com.sunnyweather.android.logic.Repository
+import com.sunnyweather.android.logic.model.Place
+
+class PlaceViewModel : ViewModel() {
+
+    private val searchLiveData = MutableLiveData<String>()
+
+    val placeList = ArrayList<Place>()
+
+    val placeLiveData = switchMap(searchLiveData) { query ->
+        Repository.searchPlaces(query)
+    }
+
+    fun searchPlaces(query: String) {
+        searchLiveData.value = query
+    }
+}
